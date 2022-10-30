533a86cfc653e5cb5a55ebb2f2492baec3e0eb0f
Peterson <petersoncabrini@hotmail.com>
2022-10-30 14:24:25 -0300

    add new credit card type, that changes logo and bg

--- src/main.js
+++ src/main.js
@@ -3,19 +3,32 @@ import IMask from 'imask';
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
-const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
+const background =  document.getElementById("cc");
 
 function setCardType(type) {
 
     const colors = {
         "visa": ["#436D99", "#2D57F2"],
         "mastercard": ["#DF6F29", "#C69347"],
-        "default": ["black", "gray"]
+        "default": ["black", "gray"],
+        "futurama": ["", ""]
     }
 
     ccBgColor01.setAttribute("fill", colors[type][0])
     ccBgColor02.setAttribute("fill", colors[type][1])
-    ccLogo.setAttribute("src", `cc-${type}.svg`)
+
+    if (type == "futurama") {
+        ccLogo.setAttribute("src", `cc-${type}.png`)
+        ccLogo.setAttribute("class", "futurama-logo") 
+        background.style.backgroundImage = "url('futuramaBg.png')";
+        background.style.borderRadius = "6%"
+    } else
+    {
+        ccLogo.setAttribute("src", `cc-${type}.svg`)   
+        background.style.backgroundImage = "url('cc-bg.svg')";
+    }
+   
 }
 
 setCardType("default")
@@ -57,6 +70,11 @@ const cardNumberPattern = {
             mask: "0000 0000 0000 0000",
             regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
             cardType: 'mastercard'
+        },
+        {
+            mask: "0000 0000 0000 0000",
+            regex: /^3[47]\d{0,14}$/,
+            cardType: 'futurama'
         },
         {
             mask: "0000 0000 0000 0000",
